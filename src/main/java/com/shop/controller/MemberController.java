ec1aac4f8af0c813fe4040c752e346132187276d
hisunho <peller02@naver.com>
2023-03-08 15:13:08 +0900

    168 회원가입 유효성 검증 완료

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
 @RequestMapping("/members")
 @Controller
 @RequiredArgsConstructor
@@ -30,5 +32,20 @@ public class MemberController {
 
         return "redirect:/";
 
+    }
+
+    @PostMapping(value = "/new")
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+        return "redirect:/";
     }
 }
--- src/main/java/com/shop/dto/MemberFormDto.java
+++ src/main/java/com/shop/dto/MemberFormDto.java
@@ -2,12 +2,22 @@ package com.shop.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Email;
+
+
 
 @Getter
 @Setter
 public class MemberFormDto {
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
+    @NotBlank(message = "주소는 필수 입력 값입니다.")
     private String address;
 }
